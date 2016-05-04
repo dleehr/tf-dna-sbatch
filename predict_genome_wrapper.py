dcdfbdbc26e7aa35ea8a566e28bbed2f123f9b9e
Dan Leehr <dan.leehr@duke.edu>
2016-05-04 14:52:36 -0400

    Refactor predict_genome_wrapper to be useful as a module

--- predict_genome_wrapper.py
+++ predict_genome_wrapper.py
@@ -1,15 +1,9 @@
-
 # coding: utf-8
-
-# In[34]:
 
 import subprocess
 import os
 import itertools
 import sys
-
-
-# In[2]:
 
 # Enumerate the model files, will be parsed
 
@@ -35,11 +29,7 @@ HisMycMax_Bound_filtered_normalized_CACGTG_1a2a3mer_format.model
 HisMycMax_Bound_filtered_normalized_CATGCG_1a2a3mer_format.model
 '''
 
-
-# In[40]:
-
 # Functions to parsae the modle file names
-
 def parse_e2f(f):
     components = f.split('_')
     protein = components[0]
@@ -65,8 +55,6 @@ def parse_hismadmax(f):
     return [protein, width, core, kmers]
 
 
-# In[41]:
-
 models = list()
 for f in e2f_files.splitlines():
     models.append([f] + parse_e2f(f))
@@ -84,52 +72,57 @@ for x in range(1,23):
     chroms.append('chr{}'.format(x))
 chroms.extend(['chrX','chrY'])
 
-
-# In[57]:
-
-combinations = [x for x in itertools.product(genomes, chroms, models)]
-
-idx = int(sys.argv[1])
-vals = combinations[idx]
-
-assembly = vals[0]
-chrom = vals[1]
-params = vals[2]
-
-model_filename = params[0]
-protein = params[1]
-width = params[2]
-core = params[3]
-kmers_list = params[4]
-
-# predict_genome.py
-#usage: predict_genome.py [-h] -g GenomeFile [--chroms [Chroms [Chroms ...]]]
-#                         -m ModelFile -c Core -w Width -k Kmers [Kmers ...]
-#                         [-i] -o OutputFile
-
 # CONSTANTS
 genome_files_dir = '/data/sciencesupport/tf-dna-predictions/genomes'
 model_files_dir = '/data/sciencesupport/tf-dna-predictions/models/Ning/TF_general/SVR'
-genome_file = '{}/{}.fa'.format(genome_files_dir, assembly)
-model_file = '{}/{}'.format(model_files_dir, model_filename)
 output_base = '/data/sciencesupport/tf-dna-predictions/results'
-output_dir = '{}/{}/{}'.format(output_base, assembly, protein)
-output_file = '{}/{}_{}_predictions.bed'.format(output_dir, model_filename, chrom)
 
-# core set earlier
-# width set earlier
+def output_file_name(output_dir, model_filename, chrom):
+    return '{}/{}_{}_predictions.bed'.format(output_dir, model_filename, chrom)
 
-# predict_genome
-command = ['python', 'predict_genome.py', 
-           '-g', genome_file, '--chroms', chrom, 
-           '-m', model_file, '-c', core, 
-           '-w', str(width), '-k']
-command.extend(kmers_list)
-command.extend(['-o', output_file])
+def output_dir_name(output_base, assembly, protein):
+    return '{}/{}/{}'.format(output_base, assembly, protein)
 
-try:
-   os.makedirs(output_dir)
-except OSError as e:
-   pass # Tried to check if not exists first, but when we schedule multiple simultaneous jobs, it fails.
-print ' '.join(command)
+if __name__ == '__main__':
+    combinations = [x for x in itertools.product(genomes, chroms, models)]
+
+    idx = int(sys.argv[1])
+    vals = combinations[idx]
+
+    assembly = vals[0]
+    chrom = vals[1]
+    params = vals[2]
+
+    model_filename = params[0]
+    protein = params[1]
+    width = params[2]
+    core = params[3]
+    kmers_list = params[4]
+
+    # predict_genome.py
+    #usage: predict_genome.py [-h] -g GenomeFile [--chroms [Chroms [Chroms ...]]]
+    #                         -m ModelFile -c Core -w Width -k Kmers [Kmers ...]
+    #                         [-i] -o OutputFile
+
+    genome_file = '{}/{}.fa'.format(genome_files_dir, assembly)
+    model_file = '{}/{}'.format(model_files_dir, model_filename)
+    output_dir = output_dir_name(output_base, assembly, protein)
+    output_file = output_file_name(output_dir, model_filename, chrom)
+
+    # core set earlier
+    # width set earlier
+
+    # predict_genome
+    command = ['python', 'predict_genome.py',
+               '-g', genome_file, '--chroms', chrom,
+               '-m', model_file, '-c', core,
+               '-w', str(width), '-k']
+    command.extend(kmers_list)
+    command.extend(['-o', output_file])
+
+    try:
+       os.makedirs(output_dir)
+    except OSError as e:
+       pass # Tried to check if not exists first, but when we schedule multiple simultaneous jobs, it fails.
+    print ' '.join(command)
 
