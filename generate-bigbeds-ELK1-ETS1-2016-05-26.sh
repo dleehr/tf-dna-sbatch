51cc9a3952c4cb8527ac3c6c4a452b30efd09502
Dan Leehr <dan.leehr@duke.edu>
2016-05-26 15:05:22 -0400

    Use no-resize for ELK/ETS1 bigbeds

--- generate-bigbeds-ELK1-ETS1-2016-05-26.sh
+++ generate-bigbeds-ELK1-ETS1-2016-05-26.sh
@@ -6,4 +6,4 @@
 cd /home/dcl9/TrackHubGenerator
 module load Anaconda/2.1.0-fasrc01
 source activate env
-srun run-json-jobs.sh /home/dcl9/tf-dna-sbatch/json-jobs-2016-05-26/ /data/sciencesupport/tf-dna-predictions/results/bigbeds/
+srun run-json-jobs-no-resize.sh /home/dcl9/tf-dna-sbatch/json-jobs-2016-05-26/ /data/sciencesupport/tf-dna-predictions/results/bigbeds/
