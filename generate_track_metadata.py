50f0dcccc4088305ef7689def5f946ec19e4b6eb
Dan Leehr <dan.leehr@duke.edu>
2016-08-10 13:42:15 -0400

    Adds transform flag to tracks

--- generate_track_metadata.py
+++ generate_track_metadata.py
@@ -3,7 +3,7 @@ import os
 from generate_json_jobs import FILTER_THRESHOLDS, ORDERED_PROTEINS
 import yaml
 
-def make_metadata_dict(assembly, track_filename, track_name, model_filenames, author_identifier, serial_number, filter_threshold, protein, cores, kmers, width, slope_intercept):
+def make_metadata_dict(assembly, track_filename, track_name, model_filenames, author_identifier, serial_number, filter_threshold, protein, cores, kmers, width, slope_intercept, transform):
     m = dict()
     m['assembly'] = assembly
     m['track_filename'] = track_filename
@@ -17,6 +17,7 @@ def make_metadata_dict(assembly, track_filename, track_name, model_filenames, au
     m['kmers'] = kmers
     m['width'] = width
     m['slope_intercept'] = slope_intercept
+    m['transform'] = transform
     return m
 
 
@@ -37,8 +38,9 @@ def write_yaml(file_name):
             kmers = [3]
             width = 36
             slope_intercept = True
+            transform = False
             track_name = '{}_{}({})'.format(protein, serial_number, author_identifier)
-            metadata = make_metadata_dict(assembly, track_filename, track_name, [model_filename], author_identifier, serial_number, filter_threshold, protein, cores, kmers, width, slope_intercept)
+            metadata = make_metadata_dict(assembly, track_filename, track_name, [model_filename], author_identifier, serial_number, filter_threshold, protein, cores, kmers, width, slope_intercept, transform)
             metadata_dicts.append(metadata)
             model_number += 1
         # Tracks from NS - one track per protein. Protein, core, width, kmers encoded in the model filename
@@ -55,7 +57,12 @@ def write_yaml(file_name):
             cores = [model[3] for model in models]
             kmers = [int(x) for x in models[0][4]] #kmers must be consistent for all models on a protein
             slope_intercept = False
-            metadata = make_metadata_dict(assembly, track_filename, track_name,  model_filenames, author_identifier, serial_number, filter_threshold, protein, cores, kmers, width, slope_intercept)
+            # Assign to literal values so that yaml doesn't use references
+            if model[4]:
+              transform = True
+            else:
+              transform = False
+            metadata = make_metadata_dict(assembly, track_filename, track_name,  model_filenames, author_identifier, serial_number, filter_threshold, protein, cores, kmers, width, slope_intercept, transform)
             metadata_dicts.append(metadata)
             model_number += 1
     with open(file_name, 'w') as f:
