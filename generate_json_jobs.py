719b15cf8b73990b104084b5b3f4c0484d786195
Dan Leehr <dan.leehr@duke.edu>
2016-09-12 09:51:32 -0400

    Reorders proteins in generate_json_jobs
    - adds model_key to match model name with proper protein name
    - adjusts output file names based on conventions agreed 2016-09-09

--- generate_json_jobs.py
+++ generate_json_jobs.py
@@ -3,20 +3,19 @@ import os
 import json
 
 ORDERED_PROTEINS_THRESHOLDS = [
-  {'protein':'E2F1', 'filter_threshold': 0.1933},
-  {'protein':'E2F4', 'filter_threshold': 0.1620},
-  {'protein':'ELK1', 'filter_threshold': 0.2244},
-  {'protein':'ETS1', 'filter_threshold': 0.2128},
-  {'protein':'HisMadMax', 'filter_threshold': 0.1837},
-  {'protein':'HisMycMax', 'filter_threshold': 0.1619},
-  {'protein':'E2F3', 'filter_threshold': 0.2762},
-  {'protein':'GABPA', 'filter_threshold': 0.2888},
-  {'protein':'HisMax', 'filter_threshold': 0.2386},
-  {'protein':'Runx1', 'filter_threshold': 0.3061},
-  {'protein':'Runx2', 'filter_threshold': 0.2711},
+  {'protein':'c-Myc', 'model_key': 'HisMycMax', 'filter_threshold': 0.1619},
+  {'protein':'Mad1', 'model_key': 'HisMadMax', 'filter_threshold': 0.1837},
+  {'protein':'Max', 'model_key': 'HisMax', 'filter_threshold': 0.2386},
+  {'protein':'Elk1', 'model_key': 'ELK1', 'filter_threshold': 0.2244},
+  {'protein':'Ets1', 'model_key': 'ETS1', 'filter_threshold': 0.2128},
+  {'protein':'Gabpa', 'model_key': 'GABPA', 'filter_threshold': 0.2888},
+  {'protein':'E2f1', 'model_key': 'E2F1', 'filter_threshold': 0.1933},
+  {'protein':'E2f3', 'model_key': 'E2F3', 'filter_threshold': 0.2762},
+  {'protein':'E2f4', 'model_key': 'E2F4', 'filter_threshold': 0.1620},
+  {'protein':'Runx1', 'model_key': 'Runx1', 'filter_threshold': 0.3061},
+  {'protein':'Runx2', 'model_key': 'Runx2', 'filter_threshold': 0.2711},
 ]
 
-ORDERED_PROTEINS = [x['protein'] for x in ORDERED_PROTEINS_THRESHOLDS ]
 FILTER_THRESHOLDS = { x['protein']: x['filter_threshold'] for x in ORDERED_PROTEINS_THRESHOLDS }
 
 def make_job_dict(input_file_paths, filter_threshold, assembly, intermediate_output_file_name, output_bigbed_file_name):
@@ -35,7 +34,8 @@ def make_job_dict(input_file_paths, filter_threshold, assembly, intermediate_out
 def main(start_protein_number):
     for assembly in pgw.genomes:
         protein_number = start_protein_number
-        for protein in ORDERED_PROTEINS:
+        for protein_dict in ORDERED_PROTEINS_THRESHOLDS:
+            protein = protein_dict['protein']
             # One set of input files per model
             formatted_protein_number = '{0:04d}'.format(protein_number)
             input_file_paths = list()
@@ -48,8 +48,8 @@ def main(start_protein_number):
                     input_file_paths.append(pgw.output_file_name(output_dir, os.path.basename(model_filename), chrom))
             # Now we have a list of source filenames
             filter_threshold = FILTER_THRESHOLDS[protein]
-            intermediate_output_file_name = '{}-{}-{}.bed'.format(assembly, formatted_protein_number, protein)
-            output_bigbed_file_name = '{}-{}-{}.bb'.format(assembly, formatted_protein_number, protein)
+            intermediate_output_file_name = '{}_{}_{}.bed'.format(assembly, formatted_protein_number, protein)
+            output_bigbed_file_name = '{}_{}_{}.bb'.format(assembly, formatted_protein_number, protein)
             job_file_name = 'json-jobs/' + os.path.splitext(intermediate_output_file_name)[0] + '.json'
             job_dict = make_job_dict(input_file_paths, filter_threshold, assembly, intermediate_output_file_name, output_bigbed_file_name)
             print 'writing to ', job_file_name
@@ -59,5 +59,5 @@ def main(start_protein_number):
 
 
 if __name__ == '__main__':
-    protein_number = 3
+    protein_number = 1
     main(protein_number)
