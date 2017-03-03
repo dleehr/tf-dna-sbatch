a0ef9ee0900bfe77a6be0d6b31226b97920a2447
Dan Leehr <dan.leehr@duke.edu>
2017-03-03 09:51:23 -0500

    Correct output dir in generate-preference-bigbeds-no-filter

--- generate-preference-bigbeds-no-filter.sh
+++ generate-preference-bigbeds-no-filter.sh
@@ -8,4 +8,4 @@ module load Anaconda/2.1.0-fasrc01
 module load R/3.2.2-fasrc03
 export PATH=$PATH:/home/dcl9/predict-tf-preference
 cd /home/dcl9/TrackHubGenerator
-srun run-preference-json-jobs.sh /home/dcl9/tf-dna-sbatch/json-jobs-2017-03-02-preferences-no-filter/ /data/sciencesupport/tf-dna-preferences/results/bigbeds/
+srun run-preference-json-jobs.sh /home/dcl9/tf-dna-sbatch/json-jobs-2017-03-02-preferences-no-filter/ /data/sciencesupport/tf-dna-preferences/results/bigbeds-no-filter/
