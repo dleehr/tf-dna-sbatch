f4fe2312ca51386027eabd7877079d282fa2985c
Dan Leehr <dan.leehr@duke.edu>
2017-03-06 11:44:56 -0500

    For no-filter preferences, ensure correct branch

--- generate-preference-bigbeds-no-filter.sh
+++ generate-preference-bigbeds-no-filter.sh
@@ -3,9 +3,11 @@
 #SBATCH --mail-user=dan.leehr@duke.edu
 #SBATCH --mail-type=FAIL
 #SBATCH --mem=16g
-
+set -e
 module load Anaconda/2.1.0-fasrc01
 module load R/3.2.2-fasrc03
 export PATH=$PATH:/home/dcl9/predict-tf-preference
+cd /home/dcl9/predict-tf-preference
+git checkout no-filter
 cd /home/dcl9/TrackHubGenerator
 srun run-preference-json-jobs.sh /home/dcl9/tf-dna-sbatch/json-jobs-2017-03-02-preferences-no-filter/ /data/sciencesupport/tf-dna-preferences/results/bigbeds-no-filter/
